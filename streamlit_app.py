d090a8afd52c79063e14842ff7fd857faaece5f0
chandrakanth-karunakaran <137074757+chandrakanth-karunakaran@users.noreply.github.com>
2024-03-12 15:04:47 +0530

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -11,8 +10,9 @@ st.write(
 )
 
 # fetching data from db smoothies
+cnx=st.connection('snowflake')
 
-session = get_active_session()
+session = cnx.session()
 
 name_on_order=st.text_input('Name On Smoothie:')
 
