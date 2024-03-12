95d90674afe69372a61508d34e2f3cb3b323abee
chandrakanth-karunakaran <137074757+chandrakanth-karunakaran@users.noreply.github.com>
2024-03-12 16:38:42 +0530

    Update streamlit_app.py
    bug fix missing variable name

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,9 +42,9 @@ if ingredients_list:
 
         ingredients_string+=fruit_selected+' '
 
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_selected , 'SEARCH_ON'].iloc[0]
         
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.write('The search value for ', fruit_selected ,' is ', search_on, '.')
 
         st.subheader(fruit_selected+' Nutrition Information')
         
